660051ec2a34ab73f2e0ef6ba519f60ebd5e9cc2
prnella <1505827460@toceansoft.com>
2017-11-09 09:29:21 +0800

    FYX：页面提交

--- src/components/page/sub_daily.vue
+++ src/components/page/sub_daily.vue
@@ -2,94 +2,30 @@
     <div class="table">
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-menu"></i> 表格</el-breadcrumb-item>
-                <el-breadcrumb-item>Vue表格组件</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-star-off"></i> 提交报告</el-breadcrumb-item>
+                <el-breadcrumb-item>提交日报</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
-        <div class="plugins-tips">
-            vue-datasource：一个用于动态创建表格的vue.js服务端组件。
-            访问地址：<a href="https://github.com/coderdiaz/vue-datasource" target="_blank">vue-datasource</a>
+        <div>
+            <el-collapse v-model="activeNames" @change="handleChange">
+                <el-collapse-item title="调查列表 (+ 单击标题栏展开)" name="1">
+                    <el-table :data="tableData" border style="width: 100%">
+                        <el-table-column prop="number" label="序号" width="200"></el-table-column>
+                        <el-table-column prop="tname" label="调查名称" width="200"></el-table-column>
+                        <el-table-column prop="tcontent" label="调查内容" width="200"></el-table-column>
+                        <el-table-column prop="tpeople" label="考评对象" width="200"></el-table-column>
+                        <el-table-column prop="ttime" label="调查时间" width="200"></el-table-column>
+                        <el-table-column></el-table-column>
+                    </el-table>
+                </el-collapse-item>
+            </el-collapse>
         </div>
-        <datasource language="en" :table-data="getData" :columns="columns" :pagination="information.pagination"
-                :actions="actions"
-                v-on:change="changePage"
-                v-on:searching="onSearch"></datasource>
     </div>
 </template>
 
+
 <script>
-    import axios from 'axios';
-    import Datasource from 'vue-datasource';
-    export default {
-        data: function(){
-            const self = this;
-            return {
-                url: './static/datasource.json',
-                information: {
-                    pagination:{},
-                    data:[]
-                },
-                columns: [
-                    {
-                        name: 'Id',
-                        key: 'id',
-                    },
-                    {
-                        name: 'Name',
-                        key: 'name',
-                    },
-                    {
-                        name: 'email',
-                        key: 'email',
-                    },
-                    {
-                        name: 'ip',
-                        key: 'ip',
-                    }
-                ],
-                actions: [
-                    {
-                        text: 'Click',
-                        class: 'btn-primary',
-                        event(e, row) {
-                            self.$message('选中的行数： ' + row.row.id);
-                        }
-                    }
-                ],
-                query:''
-            }
-        },
-        components: {
-            Datasource
-        },
-        methods: {
-            changePage(values) {
-                this.information.pagination.per_page = values.perpage;
-                this.information.data = this.information.data;
-            },
-            onSearch(searchQuery) {
-                this.query = searchQuery;
-            }
-        },
-        computed:{
-            getData(){
-                const self = this;
-                return self.information.data.filter(function (d) {
-                    if(d.name.indexOf(self.query) > -1){
-                        return d;
-                    }
-                })
-            }
-        },
-        beforeMount(){
-            if(process.env.NODE_ENV === 'development'){
-                this.url = '/ms/table/source';
-            };
-            axios.get(this.url).then( (res) => {
-                this.information = res.data;
-            })
-        }
-    }
+
 </script>
 
-<style src="../../../static/css/datasource.css"></style>
+
