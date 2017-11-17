5698ec0c42d61bb2e80831d2da2b5dd99db8ab30
zhangxiaojun <1422330397@qq.com>
2017-11-17 11:05:50 +0800

    zxj:修改了个人信息和修改密码页面

---
+++ src/components/page/ChangePassword.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-setting"></i>修改密码</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="hr"></div>
+
+        <div class="form">
+            <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="旧的密码" prop="oldPass">
+                    <el-input v-model.number="ruleForm2.oldPass"></el-input>
+                </el-form-item>
+                <el-form-item label="新设密码" prop="pass">
+                    <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="checkPass">
+                    <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+                    <el-button @click="resetForm('ruleForm2')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+<script>
+    import ElRow from "../../../node_modules/element-ui/packages/row/src/row";
+    export default {
+        components: {ElRow}, data: function () {
+            var validatePass3 = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入现在的密码'));
+                } else {
+                    if (this.ruleForm2.oldPass !== '') {
+                        this.$refs.ruleForm2.validateField('oldPass');
+                    }
+                    callback();
+                }
+            };
+            var validatePass = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请输入密码'));
+                } else {
+                    if (this.ruleForm2.checkPass !== '') {
+                        this.$refs.ruleForm2.validateField('checkPass');
+                    }
+                    callback();
+                }
+            };
+            var validatePass2 = (rule, value, callback) => {
+                if (value === '') {
+                    callback(new Error('请再次输入密码'));
+                } else if (value !== this.ruleForm2.pass) {
+                    callback(new Error('两次输入密码不一致!'));
+                } else {
+                    callback();
+                }
+            };
+            return {
+                ruleForm2: {
+                    pass: '',
+                    checkPass: '',
+                    oldPass: ''
+                },
+                rules2: {
+                    pass: [
+                        { validator: validatePass, trigger: 'blur' }
+                    ],
+                    checkPass: [
+                        { validator: validatePass2, trigger: 'blur' }
+                    ],
+                    oldPass: [
+                        { validator: validatePass3, trigger: 'blur' }
+                    ]
+                }
+
+            };
+        },
+        methods: {
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields();
+            }
+        }
+
+    }
+</script>
+
+<style>
+    .hr {
+        border-bottom: solid 1px #5e7382;
+    }
+.form{
+    width:400px;
+
+    margin: 50px auto;
+    padding: 10px 20px ;
+}
+</style>
